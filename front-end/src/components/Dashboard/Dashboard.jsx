2594f2827108a96171bcb07594197aa998ca37ff
imen <yousriimen09@gmail.com>
2024-03-11 16:15:51 +0100

    front end

--- front-end/src/components/Dashboard/Dashboard.jsx
+++ front-end/src/components/Dashboard/Dashboard.jsx
@@ -2,28 +2,57 @@ import './Dashboard.css'
 import {PieChart, Pie,Sector,Cell} from "recharts"
 const Dashboard = () => {
     const data = [
-            {name : "Nomber de Facture vente", value:13948},
-            {name : "Nomber de Facture service", value:47277},
+            {name : "Nomber de Facture vente", value:13948, fill: '#d9b277'},
+            {name : "Nomber de Facture service", value:47277,fill:'#a7cae6'},
     ]
     return (
         <>
+        <div style={{display:'inline-block'}}>
+
+        
         <div className='card'>
             <p className='container'>Factures</p>
             {/* <button>More Info</button> */}
         </div>
-        <div className='statistic'>
-        <PieChart width={400} height={400}>
+
+        <div className='statistic bg-dark'>
+        <PieChart width={200} height={200}>
         <Pie
             dataKey="value"
             isAnimationActive={false}
             data={data}
-            cx="50%"
-            cy="50%"
-            outerRadius={80}
-            fill="#8884d8"
-            label
+            cx="40%"
+            cy="30%"
+            outerRadius={30}
+            label ={{fontSize: 10,}}
         />
         </PieChart>
+
+        </div>
+        </div>
+
+        <div style={{display:'inline-block'}}>
+
+        
+        <div className='card'>
+            <p className='container'>Factures</p>
+            {/* <button>More Info</button> */}
+        </div>
+
+        <div className='statistic bg-dark'>
+        <PieChart width={200} height={200}>
+        <Pie
+            dataKey="value"
+            isAnimationActive={false}
+            data={data}
+            cx="40%"
+            cy="30%"
+            outerRadius={30}
+            label ={{fontSize: 10,}}
+        />
+        </PieChart>
+
+        </div>
         </div>
         </>
     )
